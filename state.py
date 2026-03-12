f914ba9f0ddd8593eee4c16c7336adf212ba1b81
edda-mol <s250265@dtu.dk>
2026-03-12 21:56:57 +0100

    created file state.py

---
+++ state.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass, field
+from typing import List, Optional, Literal
+
+import classes # Uses graph, player, deck, route_card as defined there
+
+#___________________________________
+# Action Representation
+#___________________________________
+
+# Define a class Action that lets a player draw or claim a route
+
+
+
+#___________________________________
+# Player State
+#___________________________________
+
+# Define a class PlayerState to track their cards on hand, ticket, and trains left (search and evaluation will use this)
+
+
+
+
+#___________________________________
+# Game State
+#___________________________________
+
+# Define a class GameState to track the game state (rules, evaluation, and search will use this)
+
