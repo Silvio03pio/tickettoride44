e1d7c1e6212a29285c9e77066d6895729af1cb38
edda-mol <s250265@dtu.dk>
2026-03-13 17:27:44 +0100

    nothing

--- rules.py
+++ rules.py
@@ -3,3 +3,21 @@
 # 2. Implement draw_card(state, player_idx)
 # 3. Implement legal_actions(state, player_idx)
 # 4. Deprecate or thin out: player.place_trains, player.draw_card, player.claim_path so they become light wrappers (or remove once all call sites are updated)
+
+
+
+
+# For testing purposes
+
+def main():
+    test_graph = graph()
+    test_graph.import_graph("ttr_europe_map_data.csv")
+
+    print("Nodes:")
+    print(test_graph.get_nodes())
+
+    print("\nPaths:")
+    print(test_graph.get_paths()[:10])  # print first 10 paths
+
+    print("\nN:")
+    print(test_graph.N)
